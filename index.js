0cd302f0ea6f666f9f8cae6fc8b462fb8f546312
bforbit <115847484+bforbit@users.noreply.github.com>
2024-06-07 12:00:00 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,110 @@
+import { scentData } from '/scentdata.js'
+
+const notesRadio = document.getElementById('notes-radio')
+const getFragranceBtn = document.getElementById('get-fragrance-btn')
+const clearBtn = document.getElementById('clear-btn')
+const fragranceImageDiv = document.getElementById('fragrance-image')
+const fragranceImageInner = document.getElementById('fragrance-image-inner')
+
+// CREATE AN ARRAY CONTAINING INDIVIDUAL FRAGRANCE NOTES
+function getNotesArray(fragrances){
+   const notesArray = []
+   
+   for (let fragrance of fragrances){
+      for (let note of fragrance.notes){
+         if (!notesArray.includes(note)){
+            notesArray.push(note)
+         }
+      }
+   }
+   return notesArray
+}
+
+// RENDER NOTES ARRAY IN HTML, SORTED
+function renderNotesRadios(fragrances){
+   let radioItems = ``
+
+   const notes = getNotesArray(fragrances)
+   notes.sort()
+
+   for (let note of notes){
+      radioItems += `
+      <div class="radio">
+         <label for="${note}">${note}</label>
+         <input type="radio"
+         id="${note}"
+         value="${note}"
+         name="notes"
+         >
+         </div>
+      `
+   }
+   notesRadio.innerHTML = radioItems
+}
+renderNotesRadios(scentData)
+
+//// GET FRAGRANCES WHEN MAIN BUTTON IS CLICKED - MULTI-STEP
+getFragranceBtn.addEventListener('click', renderImage)
+
+// EVENT LISTENER TO CHANGE LABEL BACKGROUND COLOR WHEN CLICKED
+const labelColors = document.querySelectorAll('.radio label')
+labelColors.forEach(label => {
+   label.addEventListener('click', function() {
+
+      labelColors.forEach(label => {
+         label.style.backgroundColor = 'white'
+      })
+
+         this.style.background = 'rgb(255, 228, 142)'
+   });
+});
+
+// CHECKS NOTE SELECTION, FINDS FRAGRANCE MATCHES
+function getFragrances() {
+   const checkedRadio = document.querySelector('input[type="radio"]:checked')
+   if (checkedRadio) {
+      const selectedNote = checkedRadio.value
+      const matchingFragrances = scentData.filter(function(fragrance) {
+         return fragrance.notes.includes(selectedNote)
+      });
+      return matchingFragrances;
+   }
+}
+
+// GENERATE IMAGE OR CONCATENATE IMAGES 
+function renderImage(){
+   const fragrancesArray = getFragrances()
+
+   const fragranceHTML = fragrancesArray.map(fragrance => 
+      `
+      <div class="fragrance-item">
+         <img class="scent-img"
+         src="./${fragrance.url}"
+         alt="${fragrance.name}"
+         >
+         <br>${fragrance.brand}
+         <br>${fragrance.name}
+      </div>
+      `
+   ).join('')
+
+      fragranceImageInner.innerHTML = fragranceHTML
+   }
+
+
+// CLEAR RADIO BUTTON & FRAGRANCE IMAGE DIV
+clearBtn.addEventListener('click', clearRadio)
+
+function clearRadio() {
+   const radioButtons = document.querySelectorAll('input[type="radio"]')
+   radioButtons.forEach(radio => {
+      radio.checked = false
+   })
+
+labelColors.forEach(label => {
+         label.style.backgroundColor = 'white'
+      })
+
+   fragranceImageInner.innerHTML = ''
+
+}
---
+++ scentdata.js
@@ -0,0 +1,27 @@
+export const scentData = [
+
+{brand: "Anthony Hopkins",name: "Anthony Hopkins",notes: ["Bergamot","Orange Flower","Musk","Pink Pepper","Ginger","Patchouli","Honey"],url:"img/anthonyhopkins.jpg",},
+{brand: "Yves Saint Laurent",name: "Black Opium Illicit Green",notes: ["Coffee","Mandarin","Fig"],url:"img/blackopiumillicitgreen.jpg",},
+{brand: "Versace",name: "Bright Crystal",notes: ["Peony","Acajou","Musk","Yuzu","Pomegranate","Magnolia","Lotus Flower","Amber"],url:"img/brightcrystal.jpg",},
+{brand: "Coach",name: "Coach Dreams",notes: ["Orange","Gardenia","Cactus Flower","Joshua Tree","Bergamot"],url:"img/coachdreams.jpg",},
+{brand: "Valentino",name: "Donna Born in Roma Green Stravaganza",notes: ["Tea","Jasmine","Vanilla"],url:"img/donnaborninromagreenstravaganza.jpg",},
+{brand: "Fenty Beauty",name: "Fenty",notes: ["Magnolia","Musk","Tangerine","Rose"],url:"img/fenty.jpg",},
+{brand: "Carolina Herrera",name: "Good Girl",notes: ["Jasmine","Tuberose","Tonka Bean","Almond","Coffee","Bergamot","Lemon"],url:"img/goodgirl.jpg",},
+{brand: "Michael Kors",name: "Gorgeous!",notes: ["White Floral","Tobacco","Cedarwood"],url:"img/gorgeous.jpg",},
+{brand: "Burberry",name: "Hero",notes: ["Incense","Benzoin","Cedarwood","Pine"],url:"img/hero.jpg",},
+{brand: "Vincenzo Spinnato",name: "Judy",notes: ["Rose","Dark Orchid","Coriander","Gourmand","Spice"],url:"img/judy.jpg",},
+{brand: "Lancome",name: "La Vie Est Belle Iris Absolu",notes: ["Iris","Fig","Blackcurrant","Patchouli"],url:"img/lavieestbelleirisabsolu.jpg",},
+{brand: "Yves Saint Laurent",name: "Libre",notes: ["Lavender","Orange Blossom","Musk","Vanilla"],url:"img/libre.jpg",},
+{brand: "Dolce and Gabbana",name: "Light Blue",notes: ["Apple","Lemon","Bluebells","Jasmine","Rose","Bamboo"],url:"img/lightblue.jpg",},
+{brand: "Ralph Lauren",name: "Magnolia",notes: ["Magnolia","Lemon","Patchouli"],url:"img/magnolia.jpg",},
+{brand: "Tom Ford",name: "Neroli Portofino",notes: ["Neroli","Bergamot","Lemon","Amber","Citrus"],url:"img/neroliportofino.jpg",},
+{brand: "Kate Spade",name: "New York",notes: ["Strawberry","Rose","Ambrox","Cashmeran","Bergamot","Freesia"],url:"img/newyork.jpg",},
+{brand: "Tom Ford",name: "Ombre Leather",notes: ["Leather","Patchouli","Vetiver","Jasmine","Amber","Moss","Violet","Cedar","Tobacco"],url:"img/ombreleather.jpg",},
+{brand: "Ralph Lauren",name: "Orange Flower",notes: ["Orange Flower","Mandarin","Amber"],url:"img/orangeflower.jpg",},
+{brand: "Prada",name: "Paradoxe",notes: ["Neroli","Jasmine","Amber","Musk","Pear","Tangerine","Bergamot"],url:"img/paradoxe.jpg",},
+{brand: "Bvlgari",name: "Rose Goldea Blossom Delight",notes: ["Rose","Musk","Papaya","Lily of the Valley","Grapefruit","Peony"],url:"img/rosegoldeablossomdelight.jpg",},
+{brand: "Guess Originals",name: "Type 1 Bergamot and Vetiver",notes: ["Bergamot","Vetiver","Lily","Citrus","Cardamom"],url:"img/type1bergamotandvetiver.jpg",},
+{brand: "Juicy Couture",name: "Viva La Juicy",notes: ["Wild Berry","Vanilla","Jasmine","Caramel","Amber","Mandarin","Honeysuckle"],url:"img/vivalajuicy.jpg",},
+
+
+]
